7b7bc38c0f03c1c8c35fb6660fb18f8a55212daf
Elias <eliasdragon55@hotmail.com>
2020-02-09 11:43:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ThalesVRader
+# Thales VR Experience
+## Introduction
+This repository is for the use of keeping track of the VR experience made for the Thales XR Lab and Saxion XR Lab. The people on the project should be well aware of the features that is needed to be added to this project to fully realize it. Anyone new on this project should be able to understand the code and be able to continue to work on it where others left off. 
+
+Update the README.md with a new feature added or with a new design choice. This way people can understand what the repo already contains and what needs to be worked on.
+## The goal
+The goal is to create a VR Expierence that emulates the feeling of growth while trying to display the true scale and scope of Aerospace and Earth. 
+
+## The current features
+- Scaling
+
+### The MVP will contain the following features:
+- Intuitive scalling. 
+- Realistic world building.
+
+Once the above two features are realized for the MVP can there be worked on the following features.
+
+### Additional features to add when time remains:
+- Realistic Cloud Layer
+- Eye-Tracking with the VIVE headset
+- Real-Time Event System
+- Adding planes/missiles/satellites (easter egg: Elon Musk's Tesla Roadster)
+- Realistic Satellites
+
+
+### Version
+- 3D Unity 2019.2.14f1
---
+++ unity/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+/// <summary>
+/// The inputchecker has to be on both the controller and needs the RigControl to work accordingly.
+/// </summary>
+public class ControllerInput : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;
+
+    [SerializeField] private GameObject leftController = null;
+    [SerializeField] private GameObject rightController = null;
+    [SerializeField] private RigControl playerRig = null;
+
+    [HideInInspector] private SteamVR_Input_Sources leftInput = SteamVR_Input_Sources.LeftHand;
+    [HideInInspector] private SteamVR_Input_Sources rightInput = SteamVR_Input_Sources.RightHand;
+
+    void Start()
+    {
+        //sets the trakcing object
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        //checks which controllers is being used during the which sets the boolean on the respective controller to true or false when the trigger is used
+
+        if (gameObject == leftController)
+        {
+            if (SteamVR_Actions._default.Squeeze.GetAxis(leftInput) == 1)
+            {
+                playerRig.isLeftGripped = true;
+            }
+
+            else
+            {
+                playerRig.isLeftGripped = false;
+            }
+        }
+
+        else if (gameObject == rightController)
+        {
+            if (SteamVR_Actions._default.Squeeze.GetAxis(rightInput) == 1)
+            {
+                playerRig.isRightGripped = true;
+            }
+            
+            else
+            {
+                playerRig.isRightGripped = false;
+            }
+        }
+    }
+}
